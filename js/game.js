3543de43d725cdfe727323df969bf31522dfe4eb
Vica Tea <uuf.moninabyss@gmail.com>
2025-04-27 03:01:25 +0300

    туц

--- js/game.js
+++ js/game.js
@@ -1,6 +1,4 @@
-
-
-ctx.fillStyle = 'blue'; // Цвет текста
+ctx.fillStyle = 'white'; // Цвет текста
 ctx.font = '20px Arial'; // Шрифт и размер текста
 ctx.fillText('Счет: ' + score, 10, 30); // Текст и координаты
 loadProfileFromLocalStorage();
